79bae56cdeecb486b1470180ad382342f502fb7a
nokekip <kipnoke@gmail.com>
2024-01-06 01:31:55 +0300

    fix pycode style

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,10 +5,11 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status')
 def status():
     """Return json response status as json object"""
     status = {
         'status': 'OK'
-        }
+    }
     return jsonify(status)
